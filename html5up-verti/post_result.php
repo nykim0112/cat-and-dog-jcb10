d9702e139d790ba3be5fc61e3142b455eb3a2fd2
한영진 <soulstory24@naver.com>
2019-08-09 11:10:37 +0900

    1
    1

---
+++ html5up-verti/post_result.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <p>이름: <?php echo $_POST["name"]; ?></p>
+    <p>이메일: <?php echo $_POST["email"]; ?></p>
+    <p>비밀번호: <?php echo $_POST["password"]; ?></p>
+  </body>
+</html>
